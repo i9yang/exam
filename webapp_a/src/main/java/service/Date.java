8a2da8251b6ecf84bf4a4c601cfaf07453022da4
140363 <140363@emart.com>
2015-11-02 17:29:44 +0900

    아아아아앜

--- webapp_a/src/main/java/service/Date.java
+++ webapp_a/src/main/java/service/Date.java
@@ -1,16 +1,21 @@
+package service;
+
 import com.nhnent.servlet.HttpRequest;
 import com.nhnent.servlet.HttpResponse;
 import com.nhnent.servlet.SimpleServlet;
 
 import java.io.Writer;
+import java.text.SimpleDateFormat;
 
-public class Hello implements SimpleServlet{
+public class Date implements SimpleServlet {
 	@Override
 	public void service(HttpRequest req, HttpResponse res){
 		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
 			Writer writer = res.getWriter();
-			writer.write("Hello a.com : ");
-			writer.write(req.getParameter("name"));
+			writer.write("Date a.com : ");
+			writer.write(sdf.format(new java.util.Date()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
